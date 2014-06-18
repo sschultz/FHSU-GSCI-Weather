7989d0fc62fce8598395a54d3f44c0daa20e2588
Skyler Schultz <skyler.schultz@gmail.com>
2014-06-18 16:34:15 -0500

    Removed dateutil (python2 requirement) for port to python3

--- Stations/views.py
+++ Stations/views.py
@@ -4,17 +4,6 @@ from django.views.generic import View
 import Stations.models as models
 from datetime import *
 from time import mktime
-from dateutil.tz import tzutc
-
-def timestamp(ts):
-    "Return POSIX timestamp as float as per Python3"
-    _EPOCH = datetime(1970, 1, 1, tzinfo=tzutc())
-    if ts.tzinfo is None:
-        return mktime((ts.year, ts.month, ts.day,
-            ts.hour, ts.minute, ts.second,
-            -1, -1, -1)) + self.microsecond / 1e6
-    else:
-        return (ts - _EPOCH).total_seconds()
 
 def sensorView(request, station='', sensor=''):
     station_obj = models.Station.objects.get(name=station)
@@ -23,7 +12,7 @@ def sensorView(request, station='', sensor=''):
     data = models.SensorData.objects.filter(sensor=sensor_obj, timestamp__gte=last2days)
     datastr = ''
     for record in data:
-        ts = timestamp(record.timestamp)*1000 #highcharts uses unix timestamp in milliseconds
+        ts = record.timestamp.timestamp()*1000 #highcharts uses unix timestamp in milliseconds
         datastr = datastr + "[{},{}],".format(int(ts), record.val)
     return render(request, "sensor.html", {'data':datastr})
 
