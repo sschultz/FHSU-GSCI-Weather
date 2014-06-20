ba66de846bae33c9276fa02defeebdd6d255d09c
Skyler Schultz <skyler.schultz@gmail.com>
2014-06-20 16:22:48 -0500

    Started the basic homepage layout

--- Stations/views.py
+++ Stations/views.py
@@ -5,16 +5,36 @@ import Stations.models as models
 from datetime import *
 from time import mktime
 
+def celcius2fahrenheit(c):
+    return (c*9.0/5.0)+32
+
+def dataFromLast2Days(sensor):
+    last2days = datetime.now() - timedelta(2)
+    return models.SensorData.objects.filter(sensor=sensor, timestamp__gte=last2days)
+
+def sensorData2HighchartsData(data_rec, converter=None):
+    datastr = ''
+    for record in data_rec:
+        ts = record.timestamp.timestamp()*1000 #highcharts uses unix timestamp in milliseconds
+        if converter == None:
+            val = record.val
+        else:
+            val = converter(record.val)
+        datastr = datastr + "[{},{}],".format(int(ts), val)
+    return datastr
+
 def sensorView(request, station='', sensor=''):
     station_obj = models.Station.objects.get(name=station)
     sensor_obj = models.Sensor.objects.get(slug=sensor, station=station_obj)
-    last2days = datetime.now() - timedelta(2)
-    data = models.SensorData.objects.filter(sensor=sensor_obj, timestamp__gte=last2days)
-    datastr = ''
-    for record in data:
-        ts = record.timestamp.timestamp()*1000 #highcharts uses unix timestamp in milliseconds
-        datastr = datastr + "[{},{}],".format(int(ts), record.val)
+    data = dataFromLast2Days(sensor_obj)
+    datastr = sensorData2HighchartsData(data)
     return render(request, "sensor.html", {'data':datastr})
 
 def homepageView(request):
-    return render_to_response("index.html")
+    station_obj = models.Station.objects.get(name='Windfarm')
+    tmp_sensor_obj = models.Sensor.objects.get(name='Tmp_110S_5ft',
+            station=station_obj)
+
+    tmp_data = dataFromLast2Days(tmp_sensor_obj)
+    tmp_data_str = sensorData2HighchartsData(tmp_data, celcius2fahrenheit)
+    return render(request, "index.html", {'asof':'3:30 PM', 'curtemp':'80', 'tempdata':tmp_data_str})
