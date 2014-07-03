41ef55e8287082a96915a5644a68a6acf27baaa9
John Anderson <sontek@gmail.com>
2014-07-02 21:10:10 -0500

    Core formatting

--- Stations/views.py
+++ Stations/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render_to_response, render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseNotAllowed
-from django.views.generic import View
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed
 import Stations.models as models
 import json
-from datetime import *
-from time import mktime
+from datetime import datetime, timedelta
+
 
 def celcius2fahrenheit(c):
     return (c*9.0/5.0)+32
 
+
 def dataFromLast2Days(sensor):
     last2days = datetime.now() - timedelta(2)
-    return models.SensorData.objects.filter(sensor=sensor, timestamp__gte=last2days)
+    return models.SensorData.objects.filter(sensor=sensor,
+                                            timestamp__gte=last2days)
+
 
 def sensorData2HighchartsData(data_rec, converter=None):
     datastr = ''
     for record in data_rec:
-        ts = record.timestamp.timestamp()*1000 #highcharts uses unix timestamp in milliseconds
-        if converter == None:
+         #highcharts uses unix timestamp in milliseconds
+        ts = record.timestamp.timestamp()*1000
+        if converter is None:
             val = record.val
         else:
             val = converter(record.val)
@@ -31,34 +35,36 @@ def sensorDataFromObjs(sensor_objs):
         data = dataFromLast2Days(sensor)
 
         highchart_args = {}
-        highchart_args['title'] = {'text':sensor.description}
+        highchart_args['title'] = {'text': sensor.description}
 
-        xAxis = {'text':"Date/Time"}
+        xAxis = {'text': "Date/Time"}
         xAxis['type'] = "datetime"
 
-        yAxis = {'text':sensor.sensor_type}
+        yAxis = {'text': sensor.sensor_type}
 
-        series = {'name':sensor.name}
+        series = {'name': sensor.name}
         series['data'] = [[d_obj.timestamp.timestamp()*1000,
-                d_obj.val] for d_obj in data]
+                           d_obj.val] for d_obj in data]
 
-        highchart_args['xAxis']  = xAxis
-        highchart_args['yAxis']  = yAxis
+        highchart_args['xAxis'] = xAxis
+        highchart_args['yAxis'] = yAxis
         highchart_args['series'] = [series]
 
         json_str = json.dumps(highchart_args, indent=2)
-        sensors.append({'divtagid':sensor.slug, 'JSON':json_str})
+        sensors.append({'divtagid': sensor.slug, 'JSON': json_str})
 
     return sensors
 
+
 def highchartView(request, station=''):
     if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'],'<h1>405: ResponseNotAllowed</h1>')
+        return HttpResponseNotAllowed(['POST'],
+                                      '<h1>405: ResponseNotAllowed</h1>')
     if station == '':
         return HttpResponseNotFound('<h1>Unable to find station</h1>')
     #get station object
     station_obj = models.Station.objects.get(name=station)
-    if station_obj == None:
+    if station_obj is None:
         return HttpResponseNotFound('<h1>Unable to find station</h1>')
 
     request_str = request.body.decode()
@@ -83,8 +89,9 @@ def highchartView(request, station=''):
         #sensor found, append to list
         sensor_objs.append(s_obj)
         names = names + s_obj.name + ", "
-    
+
     return HttpResponse(names, content_type="text/plain")
+
 
 def stationView(request, station='', sensors=[]):
     station_obj_all = models.Station.objects.all()
@@ -93,10 +100,10 @@ def stationView(request, station='', sensors=[]):
     if station == '':
         try:
             station_obj = models.Station.objects.get(name='Windfarm')
-            if station_obj == None:
+            if station_obj is None:
                 #no default station found, fall back on first loaded station
                 station_obj = models.Station.objects.all()[0]
-                if station_obj == None:
+                if station_obj is None:
                     return HttpResponseNotFound('<h1>Default station not found</h1>')
         except:
             #if ...objects.all() returns None
@@ -104,9 +111,9 @@ def stationView(request, station='', sensors=[]):
             return HttpResponseNotFound('<h1>Default station not found</h1>')
     else:
         station_obj = models.Station.objects.get(name=station)
-    
+
     #if station is not found then return default station page
-    if station_obj == None:
+    if station_obj is None:
         return stationView(request)
 
     #get all sensors on this station
@@ -117,10 +124,12 @@ def stationView(request, station='', sensors=[]):
     #sensors is a list of sensor names to be displayed
     if len(sensors) == 0:
         #no sensors selected, find default sensors with frontPage set to True
-        curSensors = models.Sensor.objects.filter(station=station_obj, frontPage=True)
+        curSensors = models.Sensor.objects.filter(station=station_obj,
+                                                  frontPage=True)
     else:
         for sensor_name in sensors:
-            curSensors.append(models.Sensor.objects.get(station=station_obj, name=sensor_name))
+            curSensors.append(models.Sensor.objects.get(station=station_obj,
+                                                        name=sensor_name))
 
     #if no sensor matches list of sensor names
     #then return default station page
@@ -128,17 +137,18 @@ def stationView(request, station='', sensors=[]):
         return stationView(request)
 
     #Create a list of dictionary items containing JSON code and tag names for
-    #each sensors being displayed 
+    #each sensors being displayed
     sensor_data = sensorDataFromObjs(curSensors)
     return render(request, "station.html",
-            {'stations':station_obj_all, 'sensors':sensor_data})
+                  {'stations': station_obj_all, 'sensors': sensor_data})
+
 
 def homepageView(request):
     station_obj = models.Station.objects.get(name='Windfarm')
     tmp_sensor_obj = models.Sensor.objects.get(name='Tmp_110S_5ft',
-            station=station_obj)
+                                               station=station_obj)
 
     tmp_data = dataFromLast2Days(tmp_sensor_obj)
     tmp_data_str = sensorData2HighchartsData(tmp_data, celcius2fahrenheit)
     return render(request, "index.html",
-            {'asof':'3:30 PM', 'curtemp':'80', 'tempdata':tmp_data_str})
+                  {'asof': '3:30 PM', 'curtemp': '80', 'tempdata': tmp_data_str})
