40681974cabf1349fe596b56ce818efcfe072e42
Skyler Schultz <skyler.schultz@gmail.com>
2014-07-07 15:49:06 -0500

    Removed django xframe protection to allow embeding in iframe

--- FHSU_GSCI_Weather/settings.py
+++ FHSU_GSCI_Weather/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'Stations.disable.DisableCSRF',
 )
 
