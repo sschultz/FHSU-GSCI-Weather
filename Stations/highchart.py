9d4466804b2b20718c487c203adcf216e4b4b5a5
Skyler Schultz <skyler.schultz@gmail.com>
2014-07-16 15:59:27 -0500

    Added some formatting to highcharts

--- Stations/highchart.py
+++ Stations/highchart.py
@@ -22,7 +22,8 @@ def dataSince(sensor, since):
                                             timestamp__gte=since)
 
 
-def optionsFromObj(sensor_obj, start=None, end=None, maxpoints=500):
+def optionsFromObj(sensor_obj, start=None, end=None,
+                   types=('AVG', 'MIN', 'MAX'), maxpoints=500):
     """Creates a JSON string to configure a highchart object"""
     if start is None:
         last2days = datetime.now() - timedelta(2)
@@ -39,36 +40,40 @@ def optionsFromObj(sensor_obj, start=None, end=None, maxpoints=500):
 #        partition data to reduce number of points
 #        data = data.annotate(val_avg=Avg('val')).values('timestamp__range')
 
-    avgdata = data.filter(val_type='AVG')
-    mindata = data.filter(val_type='MIN')
-    maxdata = data.filter(val_type='MAX')
-
     highchart_args = {}
     highchart_args['title'] = {'text': sensor_obj.description}
 
     chart = {'renderTo': sensor_obj.slug}
 
-    xAxis = {'text': "Date/Time"}
+    xAxis = {'title': {'text': "Date/Time CST (GMT-6)"}}
     xAxis['type'] = "datetime"
 
-    yAxis = {'text': sensor_obj.sensor_type}
+    yAxis = {'title': {'text': sensor_obj.get_sensor_type_display()}}
+    lbl = {}
+    lbl['format'] = '{value} ' + sensor_obj.data_unit
+    yAxis['labels'] = lbl
 
-    avg_series = {'name': 'Average'}
-    avg_series['data'] = [[d_obj.timestamp.timestamp()*1000,
-                           d_obj.val] for d_obj in avgdata]
-
-    min_series = {'name': 'Minimum'}
-    min_series['data'] = [[d_obj.timestamp.timestamp()*1000,
-                           d_obj.val] for d_obj in mindata]
-
-    max_series = {'name': 'Maximum'}
-    max_series['data'] = [[d_obj.timestamp.timestamp()*1000,
-                           d_obj.val] for d_obj in maxdata]
+    tooltip = {'valueSuffix': sensor_obj.data_unit}
+    tooltip['valueDecimals'] = 2
+    #Create a list of series objects for each requested type of data
+    seriesList = []
+    ValType = dict(models.VALUE_TYPE)
+    for t in types:
+        try:
+            thisdat = data.filter(val_type=t)
+            series = {'name': ValType[t]}
+            series['tooltip'] = tooltip
+            series['data'] = [
+                [d_obj.timestamp.timestamp()*1000,
+                 d_obj.val] for d_obj in thisdat]
+            seriesList.append(series)
+        except models.SensorData.DoesNotExist:
+            pass
 
     highchart_args['chart'] = chart
     highchart_args['xAxis'] = xAxis
     highchart_args['yAxis'] = yAxis
-    highchart_args['series'] = [avg_series, min_series, max_series]
+    highchart_args['series'] = seriesList
 
     json_str = json.dumps(highchart_args, indent=2)
 
