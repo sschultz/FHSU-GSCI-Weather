f6319e736d7f30804e8b643dc6353e4977eb2d45
Skyler Schultz <skyler.schultz@gmail.com>
2014-09-02 11:20:06 -0500

    Added docstring to CSRF disable

--- Stations/disable.py
+++ Stations/disable.py
@@ -1,3 +1,11 @@
 class DisableCSRF(object):
+    """
+    This class disables the the use of
+    Cross Site Request Forgery protection (CSRF). CSRF stops
+    another server from displaying this site in a iframe. Since
+    this is what we are doing with the site we have to disable
+    CSRF. Normally you would disable CSRF as per view, but we
+    are disabling it for the entire site with this.
+    """
     def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
